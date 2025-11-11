c3eb822115f4afe0ffdabab6834d3f9bcd8187a4
leobravoe <leonardo.bravo.estacio@gmail.com>
2025-11-10 22:05:40 -0300

    exemplo de login

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
 
 const App = () => {
+
+    const fazLogin = async () => {
+        const dadosParaEnviar = JSON.stringify({
+            email: "leo2@leo.com.br",
+            senha: "12345678"
+        });
+        const response = await fetch("http://localhost:3000/api/usuarios/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: dadosParaEnviar
+        });
+        const data = await response.json();
+        console.log(data);
+    }
     return (
-        <div>App</div>
+        <div>
+            <button onClick={fazLogin}>Click</button>
+        </div>
     )
 }
 
