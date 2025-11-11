cc6880bc64fe434b2b7767fda82d4ea5e6e865e3
leobravoe <leonardo.bravo.estacio@gmail.com>
2025-11-10 21:58:17 -0300

    adicionando rotas

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import App from './pages/App.jsx'
+import Sobre from './pages/Sobre.jsx';
+import Contato from './pages/Contato.jsx';
+
+const router = createBrowserRouter(
+    [
+        { path: "/", element: <App /> },
+        { path: "/sobre", element: <Sobre /> },
+        { path: "/contato", element: <Contato /> },
+    ]
+);
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <App />
+        <RouterProvider router={router} />
     </StrictMode>,
 )
