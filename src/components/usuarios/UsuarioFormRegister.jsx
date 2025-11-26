2f7c749d9fc9205bd7eb1febacb9a034575409a9
leobravoe <leonardo.bravo.estacio@gmail.com>
2025-11-25 21:25:00 -0300

    Adicionando as rotas para os componentes páginas de usuarios

--- src/components/usuarios/UsuarioFormRegister.jsx
+++ src/components/usuarios/UsuarioFormRegister.jsx
@@ -1,20 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const UsuarioFormRegister = () => {
+    const [nome, setNome] = useState("");
+    const [email, setEmail] = useState("");
+    const [senha, setSenha] = useState("");
     return (
         <div className='m-2'>
             <form>
                 <div className="mb-2">
                     <label htmlFor="id-input-nome" className="form-label">Nome</label>
-                    <input type="text" id="id-input-nome" className="form-control" />
+                    <input
+                        type="text"
+                        id="id-input-nome"
+                        className="form-control"
+                        value={nome}
+                        onChange={(e) => setNome(e.target.value)}
+                    />
                 </div>
                 <div className="mb-2">
-                    <label htmlFor="id-input-email" className="form-label">email</label>
-                    <input type="email" id="id-input-email" className="form-control" />
+                    <label htmlFor="id-input-email" className="form-label">E-mail</label>
+                    <input
+                        type="email"
+                        id="id-input-email"
+                        className="form-control"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
                 </div>
                 <div className="mb-2">
-                    <label htmlFor="id-input-senha" className="form-label">senha</label>
-                    <input type="password" id="id-input-senha" className="form-control" />
+                    <label htmlFor="id-input-senha" className="form-label">Senha</label>
+                    <input
+                        type="password"
+                        id="id-input-senha"
+                        className="form-control"
+                        value={senha}
+                        onChange={(e) => setSenha(e.target.value)}
+                    />
                 </div>
                 <div>
                     <button type="submit" className='btn btn-primary'>Registar</button>
