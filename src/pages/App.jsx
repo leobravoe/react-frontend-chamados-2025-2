5e20dc05b87e75f9b62c66ded607602fb01082ce
leobravoe <leonardo.bravo.estacio@gmail.com>
2025-12-01 19:20:02 -0300

    Inicio aula login 2

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,27 +1,9 @@
 import React from 'react'
 
 const App = () => {
-
-    const fazLogin = async () => {
-        const dadosParaEnviar = JSON.stringify({
-            email: "leo2@leo.com.br",
-            senha: "12345678"
-        });
-        const response = await fetch("http://localhost:3000/api/usuarios/login", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: dadosParaEnviar
-        });
-        const data = await response.json();
-        console.log(data);
-    }
-    return (
-        <div>
-            <button onClick={fazLogin}>Click</button>
-        </div>
-    )
+  return (
+    <div>App</div>
+  )
 }
 
 export default App
