fe7cabd1b577f287e72eacb831e7726ebe22c2bc
leobravoe <leonardo.bravo.estacio@gmail.com>
2025-12-01 21:40:02 -0300

    usando o authFetch

--- src/pages/chamados/ChamadosIndex.jsx
+++ src/pages/chamados/ChamadosIndex.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useAuthFetch } from '../../auth/useAuthFetch';
 
 const ChamadosIndex = () => {
-  return (
-    <div>ChamadosIndex</div>
-  )
+    const authFetch = useAuthFetch();
+    useEffect( () => {
+        // cria uma função
+        const chamadosFetch = async () => {
+            try {
+                // Requisição para rota protegida 
+                const res = await authFetch("http://localhost:3000/api/chamados");
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        chamadosFetch(); // chama a função a primeira vez
+    }, [] );
+
+    return (
+        <div>
+            <p>Chamados list...</p>
+        </div>
+    )
 }
 
 export default ChamadosIndex
