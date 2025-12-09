88ce8fb3af2945a35883dbaae364373927671fd8
leobravoe <leonardo.bravo.estacio@gmail.com>
2025-12-08 21:15:11 -0300

    Mostrando exemplo do create com bloquei de acesso para usuarios logados

--- src/pages/chamados/ChamadosCreate.jsx
+++ src/pages/chamados/ChamadosCreate.jsx
@@ -1,6 +1,21 @@
 import React from 'react'
+import { useAuth } from '../../auth/useAuth';
+import { Navigate } from 'react-router-dom';
 
 const ChamadosCreate = () => {
+  // Estou utilizando nesse componenete as variáveis globais definidas dentro do AuthProvider
+  const auth = useAuth();
+
+  // Enquanto ainda está carregando o estado de auth, não decide redirecionar
+  if (auth.authLoading) {
+    return <p>Carregando usuário...</p>; // ou um spinner bonitinho
+  }
+
+  // Se não tiver usuário logado, redireciona declarativamente
+  if (!auth.user) {
+    return <Navigate to="/usuarios/login" replace />;
+  }
+
   return (
     <div>ChamadosCreate</div>
   )
